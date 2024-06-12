e1131ee0a5bf180da16b9962a077ae3d76d495c4
dev-jaylord <jaylordbsns000@gmail.com>
2024-06-12 14:35:33 +0800

    fix error

--- src/components/CustomButton.tsx
+++ src/components/CustomButton.tsx
@@ -1,5 +1,5 @@
 import { Button, SxProps, Theme, useTheme } from "@mui/material";
-import React, { FC } from "react";
+import { FC } from "react";
 
 type CustomButtonProps = {
   loading?: boolean;
